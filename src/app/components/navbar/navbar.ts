8db3f85c88946896c2cfd6025d165efb14b7d2c2
AntonhyBPV <antonio.benja.parra.velasquez@gmail.com>
2026-05-11 16:10:27 -0500

    Navbar - pt 2

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,6 +1,16 @@
 import { Component, HostListener, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+interface MenuItem {
+  label: string,
+  description: string,
+  icon: string
+}
+
+interface MenuData {
+  [key: string]: MenuItem[];
+}
+
 @Component({
   selector: 'app-navbar',
   imports: [CommonModule],
@@ -10,9 +20,43 @@ import { CommonModule } from '@angular/common';
 export class Navbar {
   isScrolled = signal(false);
   isHovered = signal(false);
+  activeMenu = signal<string | null>(null);
+  isMenuVisible = signal(false);
+  private closeTimeout: any;
+
+  menus: MenuData = {
+    explorar: [
+      { label: 'Convenios', description: 'Consulta los convenios activos de tu institución.', icon: 'public/icons/convenios.svg' },
+      { label: 'Empresas', description: 'Explora las empresas vinculadas al sistema.', icon: 'public/icons/empresas.svg' },
+      { label: 'Estadísticas', description: 'Visualiza datos generales del sistema.', icon: 'public/icons/estadisticas.svg' },
+    ],
+    recursos: [
+      { label: 'Guía de uso', description: 'Aprende a usar C-Loop paso a paso.', icon: 'public/icons/guia.svg' },
+      { label: 'Preguntas frecuentes', description: 'Resuelve las dudas más comunes.', icon: 'public/icons/faq.svg' },
+    ],
+    desarrolladores: [
+      { label: 'Quienes Somos', description: 'Conoce quien esta detras de C-Loop.', icon: 'public/icons/meet.svg' },
+      { label: 'Documentación', description: 'Accede a la documentación técnica de la API.', icon: 'public/icons/docs.svg' },
+      { label: 'Repositorio', description: 'Revisa el código fuente del proyecto.', icon: 'public/icons/repo.svg' },
+    ],
+  };
 
   @HostListener('window:scroll')
   onScroll() {
     this.isScrolled.set(window.scrollY > 0);
+  }
+
+  toggleMenu(menu: string) {
+    if (this.activeMenu() === menu) {
+      this.closeMenuAnimated();
+    } else {
+      this.activeMenu.set(menu);
+      setTimeout(() => this.isMenuVisible.set(true), 10);
+    }
+  }
+
+  closeMenuAnimated() {
+    this.isMenuVisible.set(false);
+    this.closeTimeout = setTimeout(() => this.activeMenu.set(null), 250);
   }
 }
